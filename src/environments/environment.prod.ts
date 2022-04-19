57e7b1bad1dc4a2451039bc2ded9efd41d391eda
Esteban Borquez <n.a.esteban.borquez@accenture.com>
2022-04-19 15:27:47 +0200

    Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAWNFuVQ6pXQ5xQ2fQhZ0jdi-jr_Z0XFDU",
+    authDomain: "moviesappangular-project.firebaseapp.com",
+    projectId: "moviesappangular-project",
+    storageBucket: "moviesappangular-project.appspot.com",
+    messagingSenderId: "806939202115",
+    appId: "1:806939202115:web:4911002bee5ef58e63faf6"
+  }
 };
